fb93d785f597080bfca244edcd3754cb97bc966d
cahernandezsol <cahernandezsol@unadvirtual.edu.co>
2026-05-26 20:29:30 -0500

    Se sube version 1 del proyecto y el readme
    Se sube version 1 del proyecto y el readme

---
+++ 213022_711_Fase_5_Camilo_Hernandez.py
@@ -0,0 +1,33 @@
+def calificacion(id,duracion,eventos):
+    if duracion > 180 and eventos > 8:
+        etiqueta="Alto"
+    elif duracion < 60 or eventos < 3:
+        etiqueta ="Bajo"
+    else:
+        etiqueta="Medio"
+
+    print(f"{id:<22} {etiqueta:<12}")
+
+def main():
+    base_datos = [
+        ["042_Pixel3", 200, 10],
+        ["123_iPhone8", 181, 9],
+        ["256_GalaxyS9", 180, 8],
+        ["307_Pixel4a", 181, 8],
+        ["410_MotoG7", 30, 5],
+        ["555_Nokia7", 120, 2],
+        ["608_Xperia5", 0, 0],
+        ["711_OnePlus7", 90, 4],
+        ["825_GalaxyA50", 120, 5],
+        ["930_PocoX3", 250, 20],
+        ["004_LGG7", 60, 3],
+        ["999_Zenfone5", 59, 10]
+    ]
+
+    print("=" * 40)
+    print(f"{'ID':<22} {'Clasificación':<12}")
+    print("=" * 40)
+
+    for i in range(len(base_datos)):
+        calificacion(base_datos[i][0],base_datos[i][1],base_datos[i][2])
+main()
---
+++ README.md
@@ -0,0 +1,6 @@
+# Fundamentos_de_programacion_Evaluacion_Final_POA
+
+Repositorio público con la entrega final de la asignatura Fundamentos de programación.
+
+Contenido
+- `213022_711_Fase_5_Camilo_Hernandez.py` : Script que clasifica sesiones por nivel de compromiso (Alto / Medio / Bajo).
